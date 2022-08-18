13d1fccadb4285d753fa2e667328b6e7b1a2fd9c
marc43 <stmarie99@gmail.com>
2022-08-18 23:07:59 +0900

    adding delete buttons

--- marc/views.py
+++ marc/views.py
@@ -116,4 +116,32 @@ def question_delete(request, question_id):
     return redirect('marc:index')
 
 
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('marc:detail', question_id=answer.question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('marc:detail', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+    context = {'answer': answer, 'form': form}
+    return render(request, 'marc/answer_form.html', context)
+
+
+@login_required(login_url='common:login')
+def answer_delete(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '삭제권한이 없습니다')
+    else:
+        answer.delete()
+    return redirect('marc:detail', question_id=answer.question.id)
+
 # https://wikidocs.net/71445
